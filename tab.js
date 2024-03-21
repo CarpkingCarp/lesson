99c350610cee3523936b400b8cd93be3e9b0d0bb
geon <goguma03012@gmail.com>
2024-03-21 18:38:18 +0900

    지금까지 한 거 수정

--- tab.js
+++ tab.js
@@ -5,14 +5,26 @@
 // - 디브0에 쇼 클래스명 추가
 
 // for 반복문은 코드를 반복실행해주는 역할을 하는군
-let 탭버튼 = $('.tab-button');
-for(let e = 0; e < 탭버튼.length; e++){
-    $('.tab-button').eq(e).on('click', function(){
-        $('.tab-button').removeClass('orange');
-        $('.tab-button').eq(e).addClass('orange');
-        $('.tab-content').removeClass('show');
-        $('.tab-content').eq(e).addClass('show');
-    })
-    
+// 함수로 축약할 때 변수가 있다면 파라미터로 변경해야 함
+// data-id로 몰래 숨기고, 이를 불러오는 식으로 함수 작성 가능
+// 이벤트버블링을 활용하면 이벤트리스너 1개만 써도 개발가능
+
+// for(let e = 0; e < $('.tab-button').length; e++){
+//     $('.tab-button').eq(e).on('click', function(){
+//         탭열기(e)
+//     })   
+// }
+
+// 이벤트 리스너 1개만 써서 탭 기능 작성
+$('.list').click(function(e){
+        탭열기(e.target.dataset.id)
+    }
+)
+
+function 탭열기(숫자){
+    $('.tab-button').removeClass('orange');
+    $('.tab-button').eq(숫자).addClass('orange');
+    $('.tab-content').removeClass('show');
+    $('.tab-content').eq(숫자).addClass('show');
 }
 
